321fdce8b3fccc893177567e822dd5d89a699d48
explodenut <explodenut@outlook.com>
2026-02-12 13:56:57 +0800

    修改土豆地雷阳光与冷却

--- Lawn_Shared/Lawn/System/GameConstants.cs
+++ Lawn_Shared/Lawn/System/GameConstants.cs
@@ -636,7 +636,7 @@ GameConstants.gZombieAllowedLevels = new ZombieAllowedLevels[(int)ZombieType.Zom
                 new PlantDefinition(SeedType.Sunflower,     null, ReanimationType.Sunflower,    1,  50, 1000, PlantSubClass.Normal,  1500, "SUNFLOWER"),
                 new PlantDefinition(SeedType.Cherrybomb,    null, ReanimationType.Cherrybomb,   3,  200,3000,PlantSubClass.Normal,  0, "CHERRY_BOMB"),
                 new PlantDefinition(SeedType.Wallnut,       null, ReanimationType.Wallnut,      2,  125, 500,PlantSubClass.Normal,  0, "WALL_NUT"),
-                new PlantDefinition(SeedType.Potatomine,    null, ReanimationType.Potatomine,   37, 0, 6000,PlantSubClass.Normal,  0, "POTATO_MINE"),
+                new PlantDefinition(SeedType.Potatomine,    null, ReanimationType.Potatomine,   37, 50, 2000,PlantSubClass.Normal,  0, "POTATO_MINE"),
                 new PlantDefinition(SeedType.Snowpea,       null, ReanimationType.Snowpea,      4,  300,2000, PlantSubClass.Shooter, 70, "SNOW_PEA"),
                 new PlantDefinition(SeedType.Chomper,       null, ReanimationType.Chomper,      31, 100,500, PlantSubClass.Normal,  0, "CHOMPER"),
                 new PlantDefinition(SeedType.Repeater,      null, ReanimationType.Repeater,     5,  400,1000, PlantSubClass.Shooter, 70, "REPEATER"),
